1ad71d412b6ae2163284676f0fff8785b7ddd849
logicguy1 <56993729+logicguy1@users.noreply.github.com>
2021-12-05 22:57:24 +0100

    Delete Day 1 directory

--- Day 1/main.py
+++
@@ -1,7 +0,0 @@
-with open("data.txt", "r") as file:
-    data = file.readlines()
-
-for x in data:
-    for y in data:
-        if int(x.strip()) + int(y.strip()) == 2020:
-            print(int(x.strip()) * int(y.strip()))
